5657ebfeafa49f76ccc2d78fa6d75d16c3e8a6df
sketchycode <jeremy@sketchyventures.com>
2018-03-03 00:15:17 -0700

    moved a bunch of stuff around for some cleanup

---
+++ Assets/Scripts/WorldStuff/WorldTile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WorldTileTypes {
+	Wall
+}
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class WorldTile : MonoBehaviour {
+	public WorldTileTypes tileType;
+
+	public SpriteRenderer spriteRenderer { get; private set; }
+
+	private void Start() {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+}
